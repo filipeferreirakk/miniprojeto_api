fc75915b2297e182a1fb4fc04175ff9c92c88116
filipeferreirakk <filipegomesfrr@gmail.com>
2026-05-19 00:26:40 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+livros = [
+    {"id": 1, "titulo": "Dom Casmurro", "autor": "Machado de Assis", "ano": 1899},
+    {"id": 2, "titulo": "O Cortico", "autor": "Aluisio Azevedo", "ano": 1890},
+]
+
 
 @app.route("/")
 def root():
