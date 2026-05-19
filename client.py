ab508b7b0a21e276067b3ef63f4283171cda6641
Denival Biotto Filho <denivaldenival@gmail.com>
2026-05-19 11:45:54 -0300

    Update client.py

--- client.py
+++ client.py
@@ -2,5 +2,10 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"
 
-resposta = requests.get(BASE_URL)
-print(resposta.json())
+
+def checar_status():
+    resposta = requests.get(f"{BASE_URL}/health")
+    print("Status do servidor:", resposta.json())
+
+
+checar_status()
